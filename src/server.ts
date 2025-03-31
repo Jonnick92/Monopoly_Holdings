da9927889d960fdf7cbe1fb8da8b1fc102e3f10e
Yolo <timm.sauer@outlook.de>
2025-03-31 10:09:07 +0200

    routes setup

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,26 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
+app.get('/player', (req, res) => {
+  // Spieler aufsetzen, Name, Farbe, Figur etc.
+});
+
+app.get('/create_game', (req, res) => {
+  // Spiel erstellen, Spieler hinzufügen, Regeln einstellen?
+});
+
+app.get('/games', (req, res) => {
+  // Laufende Spiele anzeigen
+});
+
+app.get('/play/:id', (req, res) => {
+  // Spielen
+});
+
+app.get('/api/v1/:name', (req, res) => {
+  res.send('API is working!, ' + req.params.name);
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
