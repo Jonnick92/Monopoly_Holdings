e5a8a5251e577e222c6862526f4ebb944a307205
MeiNic <nico.meissner2006@gmail.com>
2025-05-20 16:14:02 +0200

    add base of the game-view

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { GameViewComponent } from './game-view/game-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'game-view', component: GameViewComponent },
+    { path: '**', redirectTo: 'game-view' }
+];
