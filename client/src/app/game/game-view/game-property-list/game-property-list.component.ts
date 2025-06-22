0ac9764993b7028afa5bd2dfe06a3a5d3658410a
MeiNic <nico.meissner2006@gmail.com>
2025-06-22 19:58:56 +0200

    added overlay on click on property

--- client/src/app/game/game-view/game-property-list/game-property-list.component.ts
+++ client/src/app/game/game-view/game-property-list/game-property-list.component.ts
@@ -1,16 +1,20 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { GameDataService } from '../../game-data.service';
 import { Property } from '../../game.model';
 
 @Component({
   selector: 'app-game-property-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './game-property-list.component.html',
   styleUrl: './game-property-list.component.css'
 })
-export class GamePropertyListComponent {
+export class GamePropertyListComponent implements OnInit {
   gameService = inject(GameDataService);
   properties: Property[] = [];
+  selectedProperty: Property | null = null;
+  showOverlay = false;
 
   ngOnInit(): void {
     const ownProperties = this.gameService.ownProperties();
@@ -19,4 +23,22 @@ export class GamePropertyListComponent {
     } else {
       this.properties = [];
     }
-  }}
+  }
+
+  onPropertyClick(property: Property): void {
+    this.selectedProperty = property;
+    this.showOverlay = true;
+  }
+
+  closeOverlay(): void {
+    this.showOverlay = false;
+    this.selectedProperty = null;
+  }
+
+  onOverlayClick(event: Event): void {
+    // Close overlay when clicking on the background
+    if (event.target === event.currentTarget) {
+      this.closeOverlay();
+    }
+  }
+}
